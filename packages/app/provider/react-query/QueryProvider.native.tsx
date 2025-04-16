3861dbb6acb9abb57eeb05a97e7fae25d8d81e26
afonso <appachhe1337@proton.me>
2025-04-16 16:35:30 +0100

    remove old src

--- packages/app/provider/react-query/QueryProvider.native.tsx
+++
@@ -1,8 +0,0 @@
-// Remove tRPC related code, keep only React Query if you're using it
-import { QueryClient, QueryClientProvider as QueryClientProviderOG } from '@tanstack/react-query'
-
-export function QueryClientProvider({ children }: { children: React.ReactNode }) {
-  const [queryClient] = useState(() => new QueryClient())
-
-  return <QueryClientProviderOG client={queryClient}>{children}</QueryClientProviderOG>
-}
